06d30c585b5b709e7841109b67c5431d80eaa537
Krzysztof Nasuta <47270121+Nasus20202@users.noreply.github.com>
2020-03-04 08:20:04 +0100

    Update losujLiczby.cpp

--- losujLiczby.cpp
+++ losujLiczby.cpp
@@ -2,19 +2,40 @@
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    vector<int> nums;
     srand(time(NULL));
-    fstream file;
-    file.open( "dane.txt", ios::out );
-    if(file.good()){
-        for(int i = 0; i < 1000; i++){
-            int k = rand()%1000+1;
-            file << k << endl;
+    fstream fileIn, fileOut;
+    fileIn.open( "dane.txt", ios::in);
+    fileOut.open("pary.txt", ios::out);
+    int s = 0;
+    if(fileIn.good()){
+        int n;
+        while (fileIn >> n) {
+            nums.push_back(n); s++;
         }
     }
-    file.close();
+    fileIn.close();
+
+    for(int i = 0; i < s/2; i++){
+        int a, b;
+        a = rand()%nums.size();
+
+
+        fileOut << nums[a]<<" - ";
+
+        nums.erase(nums.begin()+a);
+
+        b = rand()%nums.size();
+        fileOut << nums[b]<<endl;
+        nums.erase(nums.begin()+b);
+
+    }
+
+    fileOut.close();
 
 }
